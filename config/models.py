43b64671f230e89233bb4b0a31e0aeb4c4967186
andreishr <andreighiteanu1@gmail.com>
2023-03-14 18:53:50 +0000

    Models and conditions update

--- config/models.py
+++ config/models.py
@@ -3,31 +3,37 @@ app = create_app()
 
 class Staff(db.Model):
     staff_id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(40), unique=True, nullable=False)
+    name = db.Column(db.String(40), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.Text(), nullable=False)
     role = db.Column(db.String(20), nullable=False, default='n/a')
 
 class Patient(db.Model):
     patient_id = db.Column(db.Integer, primary_key = True)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('staff.staff_id'))
     name = db.Column(db.String(40), nullable=False)
     age = db.Column(db.Integer, nullable = False)
-    role = db.Column(db.String(20), nullable=False, default='patient')
     created_at = db.Column(db.DateTime, default=datetime.now())
     
 
 class Treatment(db.Model):
     treatment_id = db.Column(db.Integer, primary_key = True)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('staff.staff_id'))
+    patient_id = db.Column(db.Integer, db.ForeignKey('patient.patient_id'))
     treatment_name = db.Column(db.Text(), nullable = False)
     description = db.Column(db.Text(), nullable = False)
-    appliedBy = db.Column(db.Integer, db.ForeignKey('staff.staff_id'))
 
 
-class Relationships(db.Model):
-    relation_id = db.Column(db.Integer, primary_key = True)
+class Assistant_assignment(db.Model):
+    assignment_id = db.Column(db.Integer, primary_key = True)
     patient_id = db.Column(db.Integer, db.ForeignKey('patient.patient_id'), nullable=False)
-    staff_id = db.Column(db.Integer, db.ForeignKey('staff.staff_id'), nullable=False)
-    treatment = db.Column(db.Integer, db.ForeignKey('treatment.treatment_id'))
+    assistant_id = db.Column(db.Integer, db.ForeignKey('staff.staff_id'), nullable=False)
+
+
+class Assistant_treatment(db.Model):
+    applied_id = db.Column(db.Integer, primary_key = True)
+    assistant_id = db.Column(db.Integer, db.ForeignKey('staff.staff_id'), nullable=False)
+    treatment_id = db.Column(db.Integer, db.ForeignKey('treatment.treatment_id'), nullable=False)
 
 
 with app.app_context():
--- config/routes/routes.py
+++ config/routes/routes.py
@@ -8,6 +8,9 @@ from ..init import db
 #Create routes blueprint
 routes_bp = Blueprint('routes_bp', __name__)
 
+'''
+Auth routes -----------------------------------------
+'''
 
 @routes_bp.route('/login', methods = ['POST'])
 def login():
@@ -19,11 +22,11 @@ def login():
     if not staff:
         return jsonify({
             'message' : 'Wrong email or password'
-        })
+        }), 400
     if not check_password_hash(staff.password, password):
         return jsonify({
             'message' : 'Wrong email or password'
-        })
+        }), 400
 
     access_token = create_access_token(identity=staff.staff_id)
     refresh_token = create_refresh_token(identity=staff.staff_id)
@@ -32,13 +35,19 @@ def login():
     })
     set_access_cookies(response, access_token)
     set_refresh_cookies(response, refresh_token)
+    
     return response, 200
 
 @routes_bp.route('/refresh')
 @jwt_required(refresh=True)
 def refresh():
     id = get_jwt_identity()
-    access_token = create_access_token
+    access_token = create_access_token(identity=id)
+    response = jsonify({
+        'token': access_token
+    })
+    set_access_cookies(response, access_token, )
+    return response
 
 
 @routes_bp.route('/logout', methods=['DELETE'])
@@ -47,43 +56,117 @@ def logout():
         'message': 'Logout successful'
     })
     unset_jwt_cookies(response)
-
     return response, 200
 
-@routes_bp.route('/patient/add', methods = ['POST'])
+
+
+'''
+App routes -----------------------------------------
+'''
+
+@routes_bp.route('/manage/add', methods=['POST'])
 @jwt_required()
-def addPatient():
+def add_employee():
+    id = get_jwt_identity()
+    staff = Staff.query.filter_by(staff_id=id).first()
+    role = staff.role
+
+    if role != 'gm':
+        return jsonify({
+            'message': 'Access forbidden!'
+        }), 401
+    
+    emp_data = request.json
+    if not emp_data:
+        return jsonify({
+            'message' : 'Bad request'
+        }), 400
+    
+    emp_name = emp_data['name']
+    emp_email = emp_data['email']
+    emp_password = generate_password_hash(emp_data['password'])
+    emp_role = emp_data['role']
+
+    if not emp_name or not emp_email or not emp_password or not emp_role:
+        return jsonify({
+            'message': 'Missing information!'
+        }), 400
+
+    if emp_role not in ['doc', 'assist']:
+        return jsonify({
+            'message': 'Invalid role'
+        }), 
+
+    new_emp = Staff(name=emp_name, email=emp_email, password=emp_password, role=emp_role)
+    db.session.add(new_emp)
+    db.session.commit()
+    
+    return jsonify(emp_data), 201
+
+@routes_bp.route('/manage/remove', methods=['POST'])
+@jwt_required()
+def removeEmployee():
+    #TODO Complete function
+    pass
+
+@routes_bp.route('/patient/add', methods=['POST'])
+@jwt_required()
+def add_patient():
     id = get_jwt_identity()
     staff = Staff.query.filter_by(staff_id=id).first()
     role = staff.role
     if role not in ['gm', 'doc']:
         return jsonify({
             'message': 'Access forbidden!'
-        })
+        }), 401
 
     patient_data = request.json
+    if not patient_data:
+        return jsonify({
+            'message' : 'Bad request'
+        }), 400
+
     patient_name = patient_data['name']
     patient_age = patient_data['age']
-    new_patient = Patient(name=patient_name, age=patient_age)
+    doc_id = patient_data['doc_id']
+
+    if not patient_name or not patient_age or not doc_id:
+        return jsonify({
+            'message': 'Missing information!'
+        }), 401
+
+    appointed_doc = Staff.query.filter_by(staff_id=doc_id).first()
+    if not appointed_doc:
+        return jsonify({
+            'message': 'Provided ID does not belong to any employee'
+        }), 401
+
+    if appointed_doc.role != 'doc':
+        return jsonify({
+            'message': 'Provided ID does not belong to a doctor'
+        }), 401
+
+    new_patient = Patient(doctor_id=doc_id, name=patient_name, age=patient_age)
     db.session.add(new_patient)
     db.session.commit()
 
     return jsonify(patient_data), 201
 
-@routes_bp.route('/patient/<int:patientId>/delete', methods = ['DELETE'])
+@routes_bp.route('/patient/<int:patientId>/delete', methods=['DELETE'])
 @jwt_required()
-def removePatient(patientId):
+def remove_patient(patientId):
     id = get_jwt_identity()
     staff = Staff.query.filter_by(staff_id=id).first()
     role = staff.role
     if role not in ['gm', 'doc']:
         return jsonify({
             'message': 'Access forbidden!'
-        })
+        }), 401
     else:
         patient = Patient.query.get(patientId)
         db.session.delete(patient)
         db.session.commit()
         return jsonify({
             'message': 'Patient removed!'
-        })
+        }), 204
+
