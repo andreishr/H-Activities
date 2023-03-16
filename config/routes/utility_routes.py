2dc36e1a81b3a6b9d9856b9fce77e4723da5c361
andreishr <andreighiteanu1@gmail.com>
2023-03-16 22:05:43 +0000

    Routes update

--- config/routes/utility_routes.py
+++ config/routes/utility_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify, make_response
-from ..models import Staff, Patient, Treatment, Treatment_patient
+from ..models import Staff, Patient, Treatment, Treatment_patient, Assistant_assignment, Assistant_treatment
 from flask_jwt_extended import (create_access_token, set_access_cookies, jwt_required, 
     get_jwt_identity, unset_jwt_cookies, create_refresh_token, set_refresh_cookies)
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -7,7 +7,6 @@ from datetime import datetime
 from ..init import db
 
 utility_routes_bp = Blueprint('utility_routes_bp', __name__)
-
 
 @utility_routes_bp.route('/give/treatment/<int:patientId>/<int:treatId>', methods=['POST'])
 @jwt_required()
@@ -84,3 +83,120 @@ def get_treat():
         'treatment_id': applied_treat.treatment_id,
         'given_by_doc_id': applied_treat.assigned_by
     })
+
+
+@utility_routes_bp.route('/assign', methods=['POST'])
+@jwt_required()
+def assign_patient():
+    id = get_jwt_identity()
+    staff = Staff.query.filter_by(staff_id=id).first()
+    role = staff.role
+    if role not in ['gm', 'doc']:
+        return jsonify({
+            'message': 'Not authorized!'
+        }), 401
+    
+    data = request.json
+    if not data:
+        return jsonify({
+            'message': 'Bad Request.'
+        }), 400
+    
+    patient_id = data['patient_id']
+    assistant_id = data['assistant_id']
+    if not patient_id or not assistant_id:
+        return jsonify({
+            'message': 'Missing Information.'
+        }), 400
+
+    patient = Patient.query.get(patient_id)
+    if not patient:
+        return jsonify({
+            'message': 'Invalid patient ID.'
+        }), 400
+    
+    assistant = Staff.query.get(assistant_id)
+    if not assistant or assistant.role != 'assist':
+        return jsonify({
+            'message': 'Invalid assistant ID.'
+        }), 400
+
+    already_assigned = Assistant_assignment.query.filter_by(patient_id=patient_id, assistant_id=assistant_id).first()
+    if already_assigned:
+        return jsonify({
+            'message': 'Assignment already exists.'
+        })
+
+    new_assignment = Assistant_assignment(patient_id=patient_id, assistant_id=assistant_id)
+    db.session.add(new_assignment)
+    db.session.commit()
+    return jsonify({
+        'assistant_id': new_assignment.assistant_id,
+        'patient_id': new_assignment.patient_id
+    })
+    
+
+@utility_routes_bp.route('/tr/applied', methods=['GET', 'POST'])
+@jwt_required()
+def applied_treatment():
+    id = get_jwt_identity()
+    staff = Staff.query.filter_by(staff_id=id).first()
+    role = staff.role
+    if role not in ['gm', 'assist']:
+        return jsonify({
+            'message': 'Not Authorized!'
+        }), 401
+    
+    if request.method == 'POST':
+        if role != 'assist':
+            return jsonify({
+                'message': 'Treatment should be applied by an assistant'
+            })
+        data = request.json
+        if not data:
+            return jsonify({
+                'message': 'Bad Request.'
+            })
+        treatment = data['treat_id']
+        if not treatment:
+            return jsonify({
+                'message': 'Missing Information!'
+            })
+
+        treatments = db.session.query(Treatment_patient.treatment_id)\
+            .join(Assistant_assignment, Assistant_assignment.patient_id == Treatment_patient.patient_id)\
+            .filter(Assistant_assignment.assistant_id == id, Treatment_patient.patient_id == Assistant_assignment.patient_id)\
+            .all() 
+
+        treatments_to_be_applied = []
+        for treat in treatments:
+            treatments_to_be_applied.append(treat.treatment_id)
+        
+        if treatment not in treatments_to_be_applied:
+            return jsonify({
+                'message': 'No assigned patients has this treatment prescribed'
+            })
+        
+        treatment_applied = Assistant_treatment(assistant_id=id, treatment_id=treatment)
+        db.session.add(treatment_applied)
+        db.session.commit()
+        return jsonify({
+            'treatment_id': treatment,
+            'assistant_id': id
+        }), 200
+
+    applied_treats = Assistant_treatment.query.all()
+    if not applied_treats:
+        return jsonify({
+            'message': 'No treatments applied by assistants'
+        })
+    
+    list_of_treats = []
+    for applied_treat in applied_treats:
+        current_treat = {}
+        current_treat['assistant_id'] = applied_treat.assistant_id
+        current_treat['treatment_id'] = applied_treat.treatment_id
+        list_of_treats.append(current_treat)
+    
+    return jsonify(list_of_treats)
+    
